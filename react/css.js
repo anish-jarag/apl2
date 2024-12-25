f867e8245768b92576214ad08ac1fe3d1b0b3e1f
Anish Jarag <111342569+anish-jarag@users.noreply.github.com>
2024-12-25 23:38:49 +0530

    Update css.js

--- react/css.js
+++ react/css.js
@@ -1,43 +1,27 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
 
-const Card = ({ title, description }) => {
-  return (
-    <div className="card">
-      <h3 className="cardTitle">{title}</h3>
-      <p className="cardDescription">{description}</p>
-    </div>
-  );
-};
-
-const Button = ({ onClick, label }) => {
-  return (
-    <button className="button" onClick={onClick}>
-      {label}
-    </button>
-  );
-};
-
-const App = () => {
-  const [message, setMessage] = useState("");
+function App() {
+  const [message, setMessage] = useState('');
 
   const handleClick = () => {
-    setMessage("Button Clicked!");
+    setMessage('Button Clicked!');
   };
 
   return (
     <div className="app">
-      <h1 className="appTitle">React Styling with CSS</h1>
-      <div className="appContent">
-        <Card
-          title="Card Title"
-          description="This is a description of the card component. You can add more text here."
-        />
-        <Button label="Click Me" onClick={handleClick} />
-        {message && <p>{message}</p>}
+      <h1 className="title">Simple React App</h1>
+
+      <div className="card">
+        <h3 className="cardTitle">Card Title</h3>
+        <p className="cardDescription">This is a simple card description.</p>
       </div>
+
+      <button className="button" onClick={handleClick}>Click Me</button>
+
+      {message && <p>{message}</p>}
     </div>
   );
-};
+}
 
 export default App;
